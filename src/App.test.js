2aa983a79c1e24687006c3d2afc619c941eec8ed
Nishant Choubisa <nishantchoubisa92@gmail.com>
2023-10-28 15:11:17 +0530

    A2-T1-Without Solution

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App Component", () => {
+  test("is rendered correctly", () => {
+    render(<App />);
+    const headingElement = screen.getByText("Let's get Started");
+    expect(headingElement).toBeInTheDocument();
+  });
+
+  test("renders the Expenses component", () => {
+    render(<App />);
+    expect(screen.getByText("Insurance")).toBeInTheDocument();
+    expect(screen.getByText("Book")).toBeInTheDocument();
+    expect(screen.getByText("Pen")).toBeInTheDocument();
+    expect(screen.getByText("Laptop")).toBeInTheDocument();
+  });
 });
---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,19 @@
+// Write your code at the relevant places in the code below
+
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.price}</div>
+      </div>
+    </Card>
+  );
+}
+
+export default ExpenseItem;
