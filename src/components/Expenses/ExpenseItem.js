cdfac73de824ffff37cb09427c5d4e55cc9d75f6
Nishant Choubisa <nishantchoubisa92@gmail.com>
2023-10-31 13:27:06 +0530

    A2-T2-With Solution

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,21 @@
 // Write your code at the relevant places in the code below
-
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+  
   function buttonClickHandler() {
-    console.log("Button clicked");
+    setTitle("New Title");
   }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
       <button onClick={buttonClickHandler}>Change Title</button>
@@ -23,3 +24,4 @@ const ExpenseItem = (props) => {
 };
 
 export default ExpenseItem;
+
